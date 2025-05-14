c3184967c0ef999d1dc64c9f878df5e8fa0fc21f
Bar1126 <barsho26@gmail.com>
2025-05-14 13:21:11 +0300

    DOM. getting elements
    started browser events

---
+++ js1.js
@@ -0,0 +1,42 @@
+'use strict';
+//DOM. getting elements
+
+//task1
+// let p1 = document.querySelector('#elem1');
+// let p2 = document.querySelector('#elem2');
+// let p3 = document.querySelector('#elem3');
+// console.log(p1);
+// console.log(p2);
+// console.log(p3);
+//in order to print the content: add .textContent to the element
+
+//task2
+// let elem = document.querySelector('#elemContent')
+// elem.style.color = 'red'
+
+// //task3 + 4
+// let elems = document.querySelectorAll('.www')
+// elems[1].style.background = 'red'
+// elems[0].style.border = '1px solid red'
+
+//task5
+// let t5 = document.getElementById('mainTitle');
+// let changeTitle = () => (t5.textContent = 'כותרת שונתה בהצלחה');
+
+//אירועי דפדפן
+
+//task1 +2
+// let h1 = document.getElementById('mainTitle');
+// let changeColor = () => (h1.style.color = 'blue')
+// let hide = () => (h1.style.display = 'none')
+// let show = () => (h1.style.display = 'block')
+
+//task3
+// let h1 = document.getElementById('mainTitle')
+// let changeText = () => (h1.textContent = 'text changed!')
+
+//task4
+// let p1 = document.getElementById('p1')
+// let hideShow = () => (p1.style.display = p1.style.display ==='none' ? 'block' : 'none')
+
+//task5
